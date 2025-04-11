64fb9d1c242d8c3d4b9d12f0ea370e1b323732f7
Cao Quang Phong <0306191152@caothang.edu.vn>
2025-04-11 16:11:34 +0700

    longest-substring-without-repeating-characters

---
+++ Solve LeetCode Daily/0003-longest-substring-without-repeating-characters/Solution.java
@@ -0,0 +1,27 @@
+// 0001. Two Sum
+// https://leetcode.com/problems/two-sum/
+
+
+
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        // If no solution is found
+        return new int[]{};
+    }
+}
+
+
