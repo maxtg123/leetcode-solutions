23773f0f78b52060b755b42509508236bef1dfd9
Cao Quang Phong <0306191152@caothang.edu.vn>
2025-05-03 23:48:54 +0700

    Add 0048-rotate-image

--- Solve LeetCode Daily/0046-permutations/Solution.java
+++ Solve LeetCode Daily/0046-permutations/Solution.java
@@ -1,26 +1,20 @@
 class Solution {
-    public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) return "0";
-        
-        int[] result = new int[num1.length() + num2.length()];
-        
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            for (int j = num2.length() - 1; j >= 0; j--) {
-                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                int sum = mul + result[i + j + 1];
-                
-                result[i + j + 1] = sum % 10;
-                result[i + j] += sum / 10;
-            }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), res);
+        return res;
+    }
+
+    void backtrack(int[] nums, List<Integer> temp, List<List<Integer>> res) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
         }
-        
-        StringBuilder sb = new StringBuilder();
-        for (int r : result) {
-            if (!(sb.length() == 0 && r == 0)) {
-                sb.append(r);
-            }
+        for (int num : nums) {
+            if (temp.contains(num)) continue;
+            temp.add(num);
+            backtrack(nums, temp, res);
+            temp.remove(temp.size() - 1);
         }
-        
-        return sb.toString();
     }
 }
